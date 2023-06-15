3ceb4bb0653079a2c9802de57d328a194b99beac
Kevin Minehart <kmineh0151@gmail.com>
2023-06-15 11:02:04 -0500

    hide patches output; fail on command failure

--- apply.sh
+++ apply.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+set -e
 
 dir="./patches/${1}"
 
-find $dir -name "*.patch" | xargs git am -q
+find $dir -name "*.patch" | xargs git am -q > /dev/null 2>&1
 
 echo "applied $(find $dir -name "*.patch" | wc -l) patches"

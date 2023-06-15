cc6ab3137d5f5dc38ca7de206129012ab5e2224b
Kevin Minehart <kmineh0151@gmail.com>
2023-06-15 12:59:19 -0500

    add committer-date-is-author-date

--- apply.sh
+++ apply.sh
@@ -3,6 +3,6 @@ set -e
 
 dir="./patches/${1}"
 
-find $dir -name "*.patch" | xargs git am -q > /dev/null 2>&1
+find $dir -name "*.patch" | xargs git am -q --committer-date-is-author-date > /dev/null 2>&1
 
 echo "applied $(find $dir -name "*.patch" | wc -l) patches"
